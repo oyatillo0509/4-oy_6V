b5c00c31a4fefadabb92ffd625df18e85c18dba6
oyatillo0509 <tilloxon0509@gmail.com>
2024-05-29 13:40:46 +0500

    fineshed

---
+++ js/index.js
@@ -0,0 +1,71 @@
+// 1-masala
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i * i);
+//   }
+// }
+
+// 2-masala
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i * i * i);
+// }
+
+// 3-masala
+
+// for (let i = 1; i <= 20; i++) {
+//   let n = 1;
+//   for (let j = 1; j <= i; j++) {
+//     n *= j;
+//   }
+//   console.log(n);
+// }
+
+// 4-masala
+
+// let a = 1;
+// let b = 30;
+// let y = 0;
+// for (let i = a; i <= b; i++) {
+//   y += i * i;
+// }
+// console.log(y);
+
+// 5-masal
+
+// let a;
+// for (let i = 2; i <= 20; i++) {
+//   a = true;
+//   for (let j = 2; j <= Math.sqrt(i); j++) {
+//     if (i % j === 0) {
+//       a = false;
+//       break;
+//     }
+//   }
+//   if (a) {
+//     console.log(i);
+//   }
+// }
+
+// 6-masala
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i * 2 < 100) {
+//     console.log(i);
+//   }
+// }
+
+// 7-masala
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 7 === 0 && i % 5 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// 8-masala
+
+
+
+
